3fc3e44ed89a4a67fb86a98bdc2ff95616fe8c85
Arun Jayapal <deostroll@gmail.com>
2010-12-12 20:58:48 +0530

    added class twearchHttp

---
+++ twearchHttp.py
@@ -0,0 +1,10 @@
+# twearchHttp class
+# 
+# takes care of sending http requests to the twitter web services and 
+# getting the search results back to us.
+
+class twearchHttp:
+	def __init__(self):
+		self.url = ''
+		self.queryString = ''
+
